7beafdafce4c5df6d1ee97b4855e82702a649d41
hiyasin <c223059@ugrad.iiuc.ac.bd>
2026-05-31 11:34:13 +0600

    Add document text detection function to Google Cloud Vision API helper

--- src/helper/googleCloudVisionApi.ts
+++ src/helper/googleCloudVisionApi.ts
@@ -22,3 +22,22 @@ export async function recognizeDocumentText(imagePath: string) {
     throw error;
   }
 }
+
+export async function detectText(filePath:string) {
+  try {
+    const [result] = await client.textDetection(filePath);
+
+    const detections = result.textAnnotations;
+
+    if (!detections || detections.length === 0) {
+      console.log("❌ No QR/Text found");
+      return;
+    }
+
+    console.log("✅ Extracted Data:");
+    console.log(detections[0].description); // main text content
+
+  } catch (error) {
+    console.error(error);
+  }
+}
